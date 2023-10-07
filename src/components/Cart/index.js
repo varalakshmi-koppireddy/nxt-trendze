9f16bb6f8e7194e2f2009100a2c76b46ccf85244
varalakshmi <vanibattula1124@gmail.com>
2023-10-07 09:27:26 +0000

    nxt Trendze app

---
+++ src/components/Cart/index.js
@@ -0,0 +1,46 @@
+import CartContext from '../../context/CartContext'
+
+import Header from '../Header'
+import CartListView from '../CartListView'
+import CartSummary from '../CartSummary'
+import EmptyCartView from '../EmptyCartView'
+
+import './index.css'
+
+const Cart = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList, removeAllCartItems} = value
+      const showEmptyView = cartList.length === 0
+
+      const onClickRemoveAllButton = () => {
+        removeAllCartItems()
+      }
+
+      return (
+        <>
+          <Header />
+          <div className="cart-container">
+            {showEmptyView ? (
+              <EmptyCartView />
+            ) : (
+              <div className="cart-content-container">
+                <h1 className="cart-heading">My Cart</h1>
+                <button
+                  className="remove-all-btn"
+                  type="button"
+                  onClick={onClickRemoveAllButton}
+                >
+                  Remove All
+                </button>
+                <CartListView />
+                <CartSummary />
+              </div>
+            )}
+          </div>
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default Cart
